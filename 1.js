06980d29054febea3fb6dc8170d19122f2f22bfb
central771 <aadiityy@gmail.com>
2025-06-29 11:46:38 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,4 @@
-<script src="https://cdn.jsdelivr.net/gh/central771/centralo@main/1.js"></script>
+cat > 1.js << 'EOF'
+var img = new Image();
+img.src = 'https://webhook.site/d8a15841-73e2-4f6b-b9f1-b0039000383b?hit=' + Date.now();
+EOF
